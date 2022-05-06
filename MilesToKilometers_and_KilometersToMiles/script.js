63670ae9056f6b9b9a8296e9e642373e6255030f
FilipKufalov <filip.kufalov@hotmail.com>
2022-05-06 21:07:41 +0200

    Distance Converter

---
+++ MilesToKilometers_and_KilometersToMiles/script.js
@@ -0,0 +1,15 @@
+let km;
+let miles;
+
+function MilesToKilometers(km)
+{
+    return km * 1.609344;
+}
+
+function KilometersToMiles(miles)
+{
+    return miles / 1.609344;
+}
+
+console.log(`The distance of 130 kms is equal to ${MilesToKilometers(130)} miles`)
+console.log(`The distance of 209 miles is equal to ${KilometersToMiles(209)} kms`)
