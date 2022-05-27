17788f736f2243efdc9544ecd72397bf5076e740
FilipKufalov <filip.kufalov@hotmail.com>
2022-05-27 19:58:41 +0200

    Calendar & getDaysInMonth Function

---
+++ CalendarWithDays/script.js
@@ -0,0 +1,16 @@
+let today = new Date();
+let CalendarYear = today.getFullYear();
+
+function getDaysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
+}
+
+MonthString = ["January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"];
+
+console.log(`Calendar for ${CalendarYear}`);
+
+for (let i = 0; i < 12; i++) {
+    let Months = getDaysInMonth(2022, i);
+    console.log(`${MonthString[i]} ${Months}`);
+}
